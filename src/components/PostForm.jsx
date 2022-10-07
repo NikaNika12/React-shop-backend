a34e41a1cf6cd733fbcf04c1d983fa44c7d8bf3e
Veronika Pastushkova <102893905+NikaNika12@users.noreply.github.com>
2022-10-07 21:14:44 +0200

    Update PostForm.jsx

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,7 +3,8 @@ import MyInput from "./UI/input/MyInput";
 import MyButton from "./UI/button/MyButton";
 
 const PostForm = ({create}) => {
-    const [post, setPost] = useState({title: '', price: ''})
+    //const {device} = useContext(Context)
+    const [post, setPost] = useState({title: '', material: '', price: '', size: '', year: ''})
     
 
     const addNewPost = (event) => {
@@ -12,16 +13,23 @@ const PostForm = ({create}) => {
             ...post, id: Date.now(),
         }
         create(newPost)
-        setPost({title:'', price: ''})
+        setPost({title: '', material: '', price: '', size: '', year: ''})
     }
 
     return (
         <form>
+            <div>Create Artwork</div>
             <MyInput
                 value={post.title}
                 onChange={event => setPost({...post, title: event.target.value})}
                 type="text"
                 placeholder="Title"
+            />
+            <MyInput
+                value={post.price}
+                onChange={event => setPost({...post, price: event.target.value})}
+                type="text"
+                placeholder="Material"
             />
             <MyInput
                 value={post.price}
@@ -35,7 +43,43 @@ const PostForm = ({create}) => {
                 type="text"
                 placeholder="Size (W x H x D)"
             />
+            <MyInput
+                value={post.price}
+                onChange={event => setPost({...post, price: event.target.value})}
+                type="text"
+                placeholder="Year"
+            />
             <MyButton onClick={addNewPost}>Add artwork</MyButton>
+//             <Dropdown className="mt-2 mb-2">
+//                         <Dropdown.Toggle>{device.selectedSubject.name || "Choose subject"}</Dropdown.Toggle>
+//                         <Dropdown.Menu>
+//                             {device.subject.map(type =>
+//                                 <Dropdown.Item
+//                                     onClick={() => device.selectedSubject(type)}
+//                                     key={type.id}
+//                                 >
+//                                     {type.name}
+//                                 </Dropdown.Item>
+//                             )}
+//                         </Dropdown.Menu>
+//                     </Dropdown>
+//             <Dropdown className="mt-2 mb-2">
+//                         <Dropdown.Toggle>{device.selectedMedium.name || "Choose medium"}</Dropdown.Toggle>
+//                         <Dropdown.Menu>
+//                             {device.medium.map(type =>
+//                                 <Dropdown.Item
+//                                     onClick={() => device.selectedMedium(type)}
+//                                     key={type.id}
+//                                 >
+//                                     {type.name}
+//                                 </Dropdown.Item>
+//                             )}
+//                         </Dropdown.Menu>
+//                     </Dropdown>
+               <MyInput
+
+                type="file"
+            />
         </form>
     );
 };
