d5f5b3e46a52f716eead49385414051404fb44fb
Veronika Pastushkova <102893905+NikaNika12@users.noreply.github.com>
2022-10-07 20:37:14 +0200

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 const Admin = () => {
+    
+    const [modal, setModal] = useState(false);
+    
+    const createPost = (newPost) => {
+      setPosts ([...posts, newPost])
+      setModal(false)
+    }
     return (
-        <h1>
-            Admin
-        </h1>
+        <h1>Admin page</h1>
+        
+        <MyButton style={{marginTop: '30px'}} onClick ={() => setModal(true)}>
+                  Add subject
+        </MyButton>
+                <MyModal visible={modal} setVisible={setModal}>
+                  <CreateSubject/> 
+                </MyModal>
+        <MyButton style={{marginTop: '30px'}} onClick ={() => setModal(true)}>
+                  Add medium
+        </MyButton>
+                <MyModal visible={modal} setVisible={setModal}>
+                  <CreateMedium/> 
+                </MyModal>
+        <MyButton style={{marginTop: '30px'}} onClick ={() => setModal(true)}>
+                  Add artwork
+        </MyButton>
+                <MyModal visible={modal} setVisible={setModal}>
+                  <PostForm create={createPost} />  
+                </MyModal>
+
     );
 };
 
